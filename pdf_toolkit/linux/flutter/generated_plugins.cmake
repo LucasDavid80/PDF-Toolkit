f1514bd56c5b93216077af25bfcc081f21a56a8a
Lucas David <lucasdavidoliveira80@gmail.com>
2026-07-08 20:50:54 -0300

    chore(setup): resolver dependencias no lock e plugins gerados

--- pdf_toolkit/linux/flutter/generated_plugins.cmake
+++ pdf_toolkit/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  pdf_combiner
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- pdf_toolkit/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pdf_toolkit/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
-import pdf_combiner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  PdfCombinerPlugin.register(with: registry.registrar(forPlugin: "PdfCombinerPlugin"))
 }
